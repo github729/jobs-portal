acb4613742e44a0b4ea23a613e01457b791ca429
ewwgit <ramakrishna@anaghasoftech.com>
2019-04-23 16:19:53 +0530

    theme setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PostJobComponent } from './post-job/post-job.component';
+import { PostResumeComponent } from './post-resume/post-resume.component';
+import { ResumeDetailsComponent } from './resume-details/resume-details.component';
+import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileUpdateComponent } from './profile-update/profile-update.component';
+import { ResumeEditComponent } from './resume-edit/resume-edit.component';
+import { CloseAccountComponent } from './close-account/close-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'job-list', component: JobListComponent },
+  { path: 'job-details', component: JobDetailsComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'post-job', component: PostJobComponent },
+  { path: 'post-resume', component: PostResumeComponent },
+  { path: 'resume-details', component: ResumeDetailsComponent },
+  { path: 'applied-jobs', component: AppliedJobsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'profile-update', component: ProfileUpdateComponent },
+  { path: 'resume-edit', component: ResumeEditComponent },
+  { path: 'delete-account', component: CloseAccountComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PostJobComponent } from './post-job/post-job.component';
+import { PostResumeComponent } from './post-resume/post-resume.component';
+import { ResumeDetailsComponent } from './resume-details/resume-details.component';
+import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileUpdateComponent } from './profile-update/profile-update.component';
+import { ResumeEditComponent } from './resume-edit/resume-edit.component';
+import { CloseAccountComponent } from './close-account/close-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    JobListComponent,
+    JobDetailsComponent,
+    SignInComponent,
+    SignUpComponent,
+    PostJobComponent,
+    PostResumeComponent,
+    ResumeDetailsComponent,
+    AppliedJobsComponent,
+    ProfileComponent,
+    ProfileUpdateComponent,
+    ResumeEditComponent,
+    CloseAccountComponent
   ],
   imports: [
     BrowserModule,
