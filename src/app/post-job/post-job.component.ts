cdcf6cb5188bf6e2c25c3c428e4da54afe6c9e5d
ewwgit <ramakrishna@anaghasoftech.com>
2019-04-23 18:40:43 +0530

    forms convertions to angular

--- src/app/post-job/post-job.component.ts
+++ src/app/post-job/post-job.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-job',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-job.component.css']
 })
 export class PostJobComponent implements OnInit {
+  jobForm: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit() {
+    this._fb.group({
+      category: [''],
+      type: ['', Validators.required],
+      title: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(5000)]],
+      location: ['', Validators.required],
+      salary: ['', Validators.required],
+      salaryType: ['', Validators.required],
+      experience: ['', Validators.required],
+      function: ['', Validators.required],
+      industry: ['', Validators.required],
+      companyName: ['', Validators.required],
+      email: ['', Validators.required],
+      mobileNumber: ['', Validators.required],
+      address: ['', Validators.required],
+      terms: ['', Validators.required]
+    });
   }
+  postJob() {
 
+  }
 }
